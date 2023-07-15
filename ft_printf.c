0ed8d3ed4332e56049595afc1b7ed71d07b97b34
cedvid <cedricvidot@gmail.com>
2023-07-15 10:36:23 +0200

    remove format check logic from main && create new func

--- ft_printf.c
+++ ft_printf.c
@@ -11,48 +11,51 @@
 /* ************************************************************************** */
 #include "ft_printf.h"
 
+static int	ret_val;
+
+void	check_id_and_print(const char *format, va_list args)
+{
+	if (*format == 'c')
+		ret_val += ft_putchar(va_arg(args, int));
+	if (*format == 's')
+		ret_val += ft_putstr(va_arg(args, char *));
+	// if (*format == 'p')
+	// 	ret_value += ft_print_ptr(va_arg(args, void *));
+	// if (*format == 'd' || *format == 'i')
+	// 	ret_value += ft_putnbr(va_arg(args, int));
+	// if (*format == 'u')
+	// 	ret_value += ft_putnbr(va_arg(args, unsigned int));
+	// if (*format == 'x')
+	// 	ret_value += ft_print_hex_low(va_arg(args, unsigned int));
+	// if (*format == 'X')
+	// 	ret_value += ft_print_hex_up(va_arg(args, unsigned int));
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	va_start(args, format);
-	int ret_val;
 
-	ret_val = 0;
 	while (*format)
 	{
 		if (*format == '%')
-			check_format
-		{
+		{	
 			format++;
-			if (*format == 'c')
-				ret_val += ft_putchar(va_arg(args, int));
-			if (*format == 's')
-				ret_val += ft_putstr(va_arg(args, char *));
 			if (*format == '%')
 				ret_val += ft_putchar('%');
-			
+			else
+			check_id_and_print(format, args);
 		}
 		else
-			ft_putchar(*format);
+			ret_val += ft_putchar(*format);
 		format++;
 	}
 	va_end(args);
 	return (ret_val);
 }
 
-// int	main(void)
-// {
-
-// 	return 0;
-// }
-
-/* %c Prints a single character.
-• %s Prints a string (as defined by the common C convention).
-• %p The void * pointer argument has to be printed in hexadecimal format.
-• %d Prints a decimal (base 10) number.
-• %i Prints an integer in base 10.
-• %u Prints an unsigned decimal (base 10) number.
-• %x Prints a number in hexadecimal (base 16) lowercase format.
-• %X Prints a number in hexadecimal (base 16) uppercase format.
-• %% Prints a percent sign.
-*/
+int	main(void)
+{
+	printf("ret:%d\n", ft_printf("Hello %%s\n", "World"));
+	return 0;
+}
