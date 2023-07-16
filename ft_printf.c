7580bf657000d4a293e666456688397c0f8a109c
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-07-16 11:51:49 +0200

    remove check for double

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cvidot <cvidot@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:57:21 by cvidot            #+#    #+#             */
-/*   Updated: 2023/07/16 10:47:50 by cvidot           ###   ########.fr       */
+/*   Updated: 2023/07/16 11:23:16 by cvidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -21,17 +21,9 @@ int	check_id_and_print(const char *format, va_list args)
 	if (*format == 's')
 		ret_val += ft_putstr(va_arg(args, char *));
 	if (*format == 'd' || *format == 'i')
-	{
-		if (va_arg(args, double))
-			return (-1);
 		ret_val += ft_putnbr(va_arg(args, int));
-	}
-	// if (*format == 'u')
-	// 	ret_val += ft_putnbr(va_arg(args, unsigned int));
 	if (*format == 'x')
 		ret_val += ft_print_hex_low(va_arg(args, unsigned int));
-	// if (*format == 'X')
-	// 	ret_val += ft_print_hex_up(va_arg(args, unsigned int));
 	return (ret_val);
 }
 
@@ -62,7 +54,9 @@ int	ft_printf(const char *format, ...)
 
 int	main(void)
 {
-	ft_printf("%x\n", 458087404);
-	printf("%x\n", 458087404);
+	int n = ft_printf("%d, %i\n", 42, 42);
+	printf("%d\n", n);
+	// printf("%d\n", ft_printf("%d, %i\n", 42, 42));
+	// printf("%d\n",printf("%d, %i\n", 42, 42));
 	return (0);
 }
