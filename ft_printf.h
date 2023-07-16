5ff403100239ffc27baa3f8500da054bfceb4ca0
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-07-16 11:20:05 +0200

    norminette fix

--- ft_printf.h
+++ ft_printf.h
@@ -1,11 +1,22 @@
-#ifndef FT_PRINTF_H 
-# define FT_PRINT_H 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvidot <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 10:48:50 by cvidot            #+#    #+#             */
+/*   Updated: 2023/07/16 11:19:31 by cvidot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdio.h>
 # include <unistd.h>
 
-int ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 int	ft_strlen(const char *s);
 int	ft_putchar(char c);
 int	ft_putstr(char const *s);
